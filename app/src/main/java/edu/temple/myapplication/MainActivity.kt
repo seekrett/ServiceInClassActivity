a748ebeb4f0584e524e239a210d47a9fc665d8a0
Yuto <63772196+seekrett@users.noreply.github.com>
2025-04-03 12:16:47 -0400

    showed timer on GUI

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,17 +5,28 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    // OUTSIDE OF ONCREATE
-    var timerBinder : TimerService.TimerBinder? = null
+    // OUTSIDE OF ONCREATE -------
+    lateinit var timerTextView : TextView
+    // handler
+    val timerHandler = Handler(Looper.getMainLooper()) {
+        it.what.toString().also { timerTextView.text = it }
+        true
+    }
 
+    // timer binder
+    var timerBinder : TimerService.TimerBinder? = null
     val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
             timerBinder = p1 as TimerService.TimerBinder
+            timerBinder!!.setHandler(timerHandler)
         }
 
         override fun onServiceDisconnected(p0: ComponentName?) {
@@ -23,9 +34,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // ON CREATE ------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        timerTextView = findViewById(R.id.textView)
 
         bindService(
             Intent(this, TimerService::class.java),
